b31eb10950ed16d6ef998fccfe311da1efb8be41
yut-kt <yut.kt59@gmail.com>
2017-11-29 21:43:07 +0900

    [update]Function order

--- dp-matching.py
+++ dp-matching.py
@@ -11,10 +11,6 @@ def validation_argv() -> None:
         if type(argv[index]) is not str:
             exit('Argument must be string')
 
-def print_table(table: List[List[Tuple[int, int, int]]]) -> None:
-    for row in table:
-        print(row)
-
 def initialize_table(word1: str, word2: str) -> List[List[Tuple[int, int, int]]]:
     # (スコア, 遷移前の座標x(row), 遷移前の座標y(column))
     table = [[(0, 0, 0)] * (len(word1) + 1) for i in range(len(word2) + 1)]
@@ -23,7 +19,6 @@ def initialize_table(word1: str, word2: str) -> List[List[Tuple[int, int, int]]]
         table[0][column] = table[0][column - 1][0] + 7, 0, column - 1
     for row in range(1, len(table)):
         table[row][0] = table[row - 1][0][0] + 7, row - 1, 0
-
     return table
 
 def calculate_cost(table: List[List[Tuple[int, int, int]]], word1: str, word2: str) -> List[List[Tuple[int, int, int]]]:
@@ -38,13 +33,16 @@ def calculate_cost(table: List[List[Tuple[int, int, int]]], word1: str, word2: s
                 table[row][column] = sorted([up_left, left, up], key=lambda x: x[0])[0]
     return table
 
+def print_table(table: List[List[Tuple[int, int, int]]]) -> None:
+    for row in table:
+        print(row)
+
 def judge_result(table: List[List[Tuple[int, int, int]]], word1: str, word2: str) -> List[Tuple[str, str]]:
     results = []
     follow = (len(word2), len(word1))
     while follow != (0, 0):
         point = table[follow[0]][follow[1]]
         route = (point[1], point[2])
-
 
         if follow[0] == route[0]:
             results.append((word1[route[1]], '欠落'))
